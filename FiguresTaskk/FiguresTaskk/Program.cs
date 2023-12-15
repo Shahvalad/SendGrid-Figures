272512b3f764a2c1d5193626697b11fe3ef5cff5
Shahvalad <shahvaladayvazov@gmail.com>
2023-12-15 21:40:57 +0400

    initial commit

---
+++ FiguresTaskk/FiguresTaskk/Program.cs
@@ -0,0 +1,78 @@
+﻿#region Task 1 
+//*
+//**
+//***
+//****
+//*****
+//******
+//*******
+//******** 
+//*********
+
+for (int i = 0; i < 6; i++)
+{                                                      // *
+    for (int j = 0; j < i; j++)                        // **
+    {                                                  // ***
+        Console.Write("*");                            // ****
+    }                                                  // *****
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+
+
+//* * * * * * *
+//*           *
+//*           *
+//*           *
+//*           *
+//* * * * * * *
+
+
+for (int i = 0; i < 6; i++)
+{
+    for (int j = 0; j < 7; j++)
+    {
+        if (i == 0 || i == 5 || j == 0 || j == 6)
+        {
+            Console.Write("*");
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+
+
+//*****
+//*****
+//*****
+//*****
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+#endregion
+
+#region Task 2
+Console.WriteLine();
+for (int i = 1; i <= 9; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j}");
+    }
+    Console.WriteLine(new string('-', 20));
+}
+#endregion
---
+++ SendGridTask/SendGridTask/Controllers/EmailController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SendGridTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SendEmail(string To, string Subject, string Name)
+        {
+            MessageSender.ExecuteAsync(To, Subject, Name).Wait();
+            return Ok("Email Sent");
+        }
+    }
+}
